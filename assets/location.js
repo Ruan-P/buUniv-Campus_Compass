4402f998ed0e91e70ff519e4685e60aea6dfd2a6
hashcoko <hashcoko@gmail.com>
2023-11-21 03:11:35 +0900

    2차 오류 수정
    Signed-off-by: hashcoko <hashcoko@gmail.com>

--- assets/location.js
+++ assets/location.js
@@ -1,52 +1,58 @@
-var map;
-document.addEventListener('DOMContentLoaded', function() {
-    navigator.geolocation.getCurrentPosition(successGps, failGps);
-});
+navigator.geolocation.getCurrentPosition(successGps, failGps);
 
 function successGps(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const container = document.getElementById("map");
-    let options = {
-        center: new kakao.maps.LatLng(lat, lng),
-        level: 3,
-    };
+  const lat = position.coords.latitude; // 경도
+  const lng = position.coords.longitude; // 위도
+  const container = document.getElementById("map");
+  let options = {
+    center: new kakao.maps.LatLng(lat, lng),
+    level: 3,
+  };
+  let map = new kakao.maps.Map(container, options);
 
-    map = new kakao.maps.Map(container, options);
-
-    document.querySelectorAll('.nav-link').forEach(item => {
-        item.addEventListener('click', handleItemClick);
-    });
+  addEventListenersToItems();
 }
 
 function failGps() {
-    console.log("Geolocation Failed");
-    // 위치 정보를 가져오지 못했을 때 지도의 초기 위치를 정할 필요가 있음
+  console.log("Geolocation Failed");
 }
 
-function handleItemClick(event) {
-    event.preventDefault();
-    var keyword = event.target.innerText;
-    searchPlaces(keyword);
-}
-
-function searchPlaces(keyword) {
-    var places = new kakao.maps.services.Places();
-    places.keywordSearch(keyword, function(data, status, pagination) {
-        if (status === kakao.maps.services.Status.OK) {
-            displayMarkers(data);
-        } else {
-            console.error('Search failed or no results found:', status);
-        }
+function addEventListenersToItems() {
+  const items = document.querySelectorAll('.nav-link');
+  items.forEach(item => {
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+      searchAndDisplay(item.textContent.trim(), map);
     });
+  });
 }
 
-function displayMarkers(places) {
-    for (var i = 0; i < Math.min(5, places.length); i++) {
-        var place = places[i];
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: new kakao.maps.LatLng(place.y, place.x)
-        });
+function searchAndDisplay(keyword, map) {
+  var ps = new kakao.maps.services.Places(); 
+  ps.keywordSearch(keyword, function(data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+      var bounds = new kakao.maps.LatLngBounds();
+      for (var i = 0; i < data.length && i < 5; i++) { // 상위 5개 결과만 표시
+        displayMarker(data[i], map, bounds);    
+      }
+      map.setBounds(bounds);
+    } else {
+      console.log("검색 결과가 없습니다");
     }
+  });
+}
+
+function displayMarker(place, map, bounds) {
+  var marker = new kakao.maps.Marker({
+    map: map,
+    position: new kakao.maps.LatLng(place.y, place.x) 
+  });
+
+  var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+  kakao.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+    infowindow.open(map, marker);
+  });
+
+  bounds.extend(new kakao.maps.LatLng(place.y, place.x));
 }
